c4b4c1c713192fcc166665bba53892b17e410f7a
microctar lecter <microctar@outlook.com>
2023-07-09 12:37:11 +0800

    chore(fish): add comments

--- .config/fish/conf.d/mail.fish
+++ .config/fish/conf.d/mail.fish
@@ -1,3 +1,3 @@
-function outlook
+function outlook -d "Open outlook in firefox"
   firefox --new-tab "https://outlook.live.com/mail/0/"
 end
--- .config/fish/conf.d/steam.fish
+++ .config/fish/conf.d/steam.fish
@@ -1,3 +1,3 @@
-function registerkey
+function registerkey -d "Register steam CD key through firefox"
   firefox --new-tab "https://store.steampowered.com/account/registerkey"
 end
