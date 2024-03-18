e658006dcbcc9db69cbc41c35214b2b0b4932711
Microctar Lecter <microctar@outlook.com>
2024-03-18 21:16:58 +0800

    feat(fish): add function
    Add registerkey function for steam on Android.

--- .config/fish/conf.d/steam.fish
+++ .config/fish/conf.d/steam.fish
@@ -1,3 +1,10 @@
-function registerkey -d "Register steam CD key through firefox"
-  firefox --new-tab "https://store.steampowered.com/account/registerkey"
+switch (uname -o)
+case GNU/Linux
+  function registerkey -d "Register steam CD key through firefox"
+    firefox --new-tab "https://store.steampowered.com/account/registerkey"
+  end
+case Android
+  function registerkey -d "Activer un produit sur Steam"
+    am start -W -a android.intent.action.VIEW -d "https://store.steampowered.com/account/registerkey"
+  end
 end
