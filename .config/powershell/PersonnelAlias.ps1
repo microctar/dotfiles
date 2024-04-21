40d98663c10453783d2171a82c40ffcb2fad76dc
Microctar <Microctar@outlook.com>
2024-04-21 21:40:47 +0800

    feat(pwsh): add bare base64 functions

--- .config/powershell/PersonnelAlias.ps1
+++ .config/powershell/PersonnelAlias.ps1
@@ -15,5 +15,27 @@ function Local:Get-NSGPriceHistory {
 	Start-Process -FilePath msedge -ArgumentList ("https://eshop-prices.com/games?q=" + ($GameName -replace ' ', '+'))
 }
 
+function Local:Read-Base64 {
+  	[OutputType([String])]
+	param(
+		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[string]$Base64String
+	)
+
+	$bytes = [Convert]::FromBase64String($Base64String)
+	return [System.Text.Encoding]::Unicode.GetString($bytes)
+}
+
+function Local:Write-Base64 {
+	[OutputType([String])]
+	param(
+		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[string]$OriginalText
+	)
+	
+	$bytes = [System.Text.Encoding]::Unicode.GetBytes($OriginalText)
+	return [Convert]::ToBase64String($bytes)
+}
+
 Set-Alias -Name l -Value List-File
 Set-Alias -Name open -Value Invoke-Item
