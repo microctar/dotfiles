b9dfd72d7fdac5b44177e13eaf5d2f8aea3a7639
Microctar <Microctar@outlook.com>
2024-04-21 14:22:06 +0800

    feat(pwsh): add funciton for ns

--- .config/powershell/PersonnelAlias.ps1
+++ .config/powershell/PersonnelAlias.ps1
@@ -2,5 +2,18 @@ function Local:List-File {
 	Get-ChildItem | Select-Object -Property "Name"
 }
 
+function Local:Open-EShop {
+	Start-Process -FilePath msedge -ArgumentList "https://store-jp.nintendo.com"
+}
+
+function Local:Get-NSGPriceHistory {
+	param(
+		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[string]$GameName
+	)
+	
+	Start-Process -FilePath msedge -ArgumentList ("https://eshop-prices.com/games?q=" + ($GameName -replace ' ', '+'))
+}
+
 Set-Alias -Name l -Value List-File
 Set-Alias -Name open -Value Invoke-Item
