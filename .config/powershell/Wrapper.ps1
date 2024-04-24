6915b28af3256002dc18142d96a9d0038ec538b7
Microctar <Microctar@outlook.com>
2024-04-24 22:44:02 +0800

    feat(pwsh): add hex command

--- .config/powershell/Wrapper.ps1
+++ .config/powershell/Wrapper.ps1
@@ -19,3 +19,30 @@ function Local:Write-Url {
 
     return [System.Web.HttpUtility]::UrlEncode($Url)
 }
+
+function Local:ConvertTo-AsciiString {
+    [OutputType([String])]
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        [string]$FromHexString,
+
+        [Parameter(ValueFromPipeline=$true)]
+        [byte[]]$FromBytes
+    )
+
+    if (![string]::IsNullOrEmpty($FromHexString)) {
+        [string]$Text = ""
+        $hexArray = if ( $FromHexString.Contains(" ") ) { $FromHexString -split ' ' } else { $FromHexString -split '(.{2})' }
+        foreach ($hex in $hexArray) {
+            if ($hex -match "^[0-9A-Za-z]+$") {
+                $Text += [char]([Convert]::ToInt16($hex, 16))
+            }
+        }
+
+        return $Text
+    }
+
+    if ($FromBytes.Length -gt 0) {
+        return [System.Text.Encoding]::ASCII.GetString($FromBytes);
+    }
+}
