9abeaedd26ee1864f32fb128d7524c74a6acd18c
Microctar <Microctar@outlook.com>
2024-05-04 13:08:55 +0800

    feat(fish): add redeemwalletcode

--- .config/fish/conf.d/steam.fish
+++ .config/fish/conf.d/steam.fish
@@ -8,3 +8,14 @@ case Android
     am start -W -a android.intent.action.VIEW -d "https://store.steampowered.com/account/registerkey"
   end
 end
+
+switch (uname -o)
+case GNU/Linux
+  function redeemwalletcode -d "Redeem a Steam Gift Card or Wallet Code"
+    firefox --new-tab "https://store.steampowered.com/account/redeemwalletcode"
+  end
+case Android
+  function redeemwalletcode -d "Utiliser une carte ou un code de portemonnaie"
+    am start -W -a android.intent.action.VIEW -d "https://store.steampowered.com/account/redeemwalletcode"
+  end
+end
