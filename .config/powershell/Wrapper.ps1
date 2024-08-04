c65e17439dbce4595efaf8285a9f6a1cb6f5db44
Microctar <Microctar@outlook.com>
2024-08-04 15:29:51 +0800

    feat(pwsh): add create-certificate

--- .config/powershell/Wrapper.ps1
+++ .config/powershell/Wrapper.ps1
@@ -58,3 +58,47 @@ function Local:ConvertTo-HexString {
 
     return ([System.BitConverter]::ToString($bytesArray) -replace '-','')
 }
+
+function Local:Create-Certificate {
+    param(
+        [Parameter(Mandatory=$false)]
+        [int]$Days = 180,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CAName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Config,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ExtFile,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CertName = "server"
+    )
+
+    if ( -Not (Get-Command openssl) ) {
+        Write-Warning -Message "Cannot find command openssl"
+        return $NULL
+    }
+
+    [string]$ca_key = "{0}.key" -f $CAName
+    [string]$ca_csr = "{0}.csr" -f $CAName
+    [string]$ca_crt = "{0}.crt" -f $CAName
+    [string]$srv_key = "{0}.key" -f $CertName
+    [string]$srv_csr = "{0}.csr" -f $CertName
+    [string]$srv_crt = "{0}.crt" -f $CertName
+    
+
+    openssl genrsa -out $ca_key 4096
+    openssl req -config $Config -key $ca_key -new -out $ca_csr
+    openssl req -x509 -new -key $ca_key -out $ca_crt -config $Config -extensions "v3_ca" -days $Days
+
+    openssl genrsa -out $srv_key 4096
+    openssl req -config $Config -key $srv_key -new -out $srv_csr
+    openssl x509 -req -CA $ca_crt -CAkey $ca_key -CAcreateserial -extfile $ExtFile -in $srv_csr -out $srv_crt -days $Days
+}
